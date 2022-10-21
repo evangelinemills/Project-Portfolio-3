4e6a730f0e82769f2badf5de9c13d4d3edf85f3f
evangelinemills <eomills.mills@gmail.com>
2022-10-21 18:08:38 +0000

    Add colour to print messages

--- hangman.py
+++ hangman.py
@@ -3,7 +3,7 @@ from words import list_of_words  #imports the list of words from words.py
 import os
 import colorama
 from colorama import Fore, Back, Style
-
+colorama.init(autoreset=True)
 
 def obtain_word():
     """
@@ -25,7 +25,7 @@ def play_game(word):
     guessed_letters = []
     guessed_words = []
     tries = 6  #number of body parts on hangman = number of guesses
-    print(f"""LETS PLAY
+    print(Fore.CYAN + """LETS PLAY
  _     _   _____   _     _   _____    __   __    _____   _     _ 
 (_)   (_) (_____) (_)   (_) (_____)  (__)_(__)  (_____) (_)   (_)
 (_)___(_)(_)___(_)(__)_ (_)(_)  ___ (_) (_) (_)(_)___(_)(__)_ (_)
@@ -36,33 +36,33 @@ def play_game(word):
     
     difficulty_level = False
     while difficulty_level is False:
-        difficulty = input("""Please select a level: E = Easy, H = Hard: """).upper()
+        difficulty = input(Fore.LIGHTGREEN_EX + """Please select a level: E = Easy, H = Hard: """).upper()
         os.system("clear")
         if difficulty == "E":
             tries = 6
-            print("You've chosen Easy, you have ", tries, "tries.")
+            print(Fore.LIGHTMAGENTA_EX + "You've chosen Easy, you have ", tries, "tries.")
             difficulty_level = True
         elif difficulty == "H":
             tries = 6
-            print("You've chosen Hard, you have ", tries, "tries.")
+            print(Fore.LIGHTMAGENTA_EX + "You've chosen Hard, you have ", tries, "tries.")
             difficulty_level = True
         else:
-            print(difficulty, "is not a difficulty!")
+            print(Fore.LIGHTYELLOW_EX + difficulty, "is not a difficulty!")
 
     print(hangman_display(tries))
     print(word_execution)
     print(" \n ")
     while not guessed and tries > 0:
-        guess = input("Guess a LETTER or WORD: ").upper()  #input for guess
+        guess = input(Fore.LIGHTRED_EX + "Guess a LETTER or WORD: ").upper()  #input for guess
         if len(guess) == 1 and guess.isalpha():  #checks guess is alphabetic
             if guess in guessed_letters:  #checks if already guessed
-                print("Oh dear! You've already guessed this letter silly!!")  
+                print(Fore.LIGHTRED_EX + "Oh dear! You've already guessed this letter silly!!")  
             elif guess not in word:  #checks if guess not in word
-                print(guess, "is not in the word...watch out! Try again!")  
+                print(Fore.LIGHTYELLOW_EX + guess, "is not in the word...watch out! Try again!")  
                 tries -= 1  #one less try
                 guessed_letters.append(guess)  #adds guessed letter to list
             else:  #checks if correct guess
-                print("Correct! You're safe,", guess, "is in the word!")  
+                print(Fore.LIGHTMAGENTA_EX + "Correct! You're safe,", guess, "is in the word!")  
                 guessed_letters.append(guess)
                 word_as_list = list(word_execution)  #converts word into list
                 indices = [i for i, letter in enumerate(word) if letter == guess]
@@ -73,21 +73,21 @@ def play_game(word):
                     guessed = True 
         elif len(guess) == len(word) and guess.isalpha():
             if guess in guessed_words:
-                print("You've guessed the word!", guess)
+                print(Fore.CYAN + "You've guessed the word!", guess)
             elif guess != word:
-                print("Oh No!", guess , " is not the word!")
+                print(Fore.BLUE + "Oh No!", guess , " is not the word!")
                 tries -= 1
                 guessed_words.append(guess)
             else:
                 guessed = True  #Checks for correct guess
                 word_execution = word
         else:
-            print("Not a valid guess!")
+            print(Fore.LIGHTBLUE_EX + "Not a valid guess!")
         print(hangman_display(tries))
         print(word_execution)
         print("\n")
     if guessed:
-        print("""Phew! You've survived a hanging today!
+        print(Fore.BLUE + """Phew! You've survived a hanging today!
                                                                          _ 
  _       _  ______  _       _          _____    _____   _     _  ______ (_)
 (_)  _  (_)(______)(_)     (_)        (_____)  (_____) (_)   (_)(______)(_)
@@ -98,7 +98,7 @@ def play_game(word):
                                                                            
     """)
     else:
-        print("Sorry, your time is up, youre out of luck! The word was " + word, "!")
+        print(Fore.LIGHTWHITE_EX + "Sorry, your time is up, youre out of luck! The word was " + word, "!")
 
 
 def hangman_display(tries):
@@ -185,7 +185,7 @@ def hangman_display(tries):
 def main():
     word = obtain_word()
     play_game(word)
-    while input("Are you brave enough to try again? (Y/N) ").upper == "Y":
+    while input(Fore.LIGHTYELLOW_EX + "Are you brave enough to try again? (Y/N) ").upper == "Y":
         word = obtain_word()
         play_game(word)
 
